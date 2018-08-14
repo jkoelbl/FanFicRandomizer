d97d2b687e25bd33d6e53a1ef48a543fdfc2fc9f
Jordan Koelbl <jkoelbl@csumb.edu>
2018-08-14 15:14:14 -0700

    commenting of story_randomizer.py

--- story_randomizer.py
+++ story_randomizer.py
@@ -1,17 +1,21 @@
 from os import listdir
 from random import randint
 
+#get and return filenames in stories path
 def get_filenames():
 	mypath = getcwd() + '\\stories'
 	return [mypath + '\\' + f for f in listdir(mypath)]
-
+	
+#read file by every 256 bytes to save on mem
 def read_file(filename):
+	byte_size = 256
 	with open(filename) as file:
-		temp = file.read(256)
+		temp = file.read(byte_size)
 		while temp:
 			print(temp, end='')
-			temp = file.read(256)
+			temp = file.read(byte_size)
 
+#get filenames, pick one at random, and read it
 def get_random_story():
 	filenames = get_filenames()
 	index = randint(0,len(filenames)-1)
