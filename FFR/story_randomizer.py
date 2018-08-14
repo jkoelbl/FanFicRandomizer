927e3d8474bfbd6204d25182c12d08544fb7e74b
Jordan Koelbl <jkoelbl@csumb.edu>
2018-08-14 15:30:11 -0700

    modified story_randomizer.py for use of global variables

--- FFR/story_randomizer.py
+++ FFR/story_randomizer.py
@@ -1,14 +1,15 @@
 from os import listdir
 from random import randint
 
+mypath = getcwd() + '\\stories'
+byte_size = 256
+
 #get and return filenames in stories path
 def get_filenames():
-	mypath = getcwd() + '\\stories'
 	return [mypath + '\\' + f for f in listdir(mypath)]
 	
 #read file by every 256 bytes to save on mem
 def read_file(filename):
-	byte_size = 256
 	with open(filename) as file:
 		temp = file.read(byte_size)
 		while temp:
