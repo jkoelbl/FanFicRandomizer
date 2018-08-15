f9e4838320f84512fa23d4809934ac85fabcc4ff
Jordan Koelbl <jkoelbl@csumb.edu>
2018-08-14 21:15:32 -0700

    added initializing batch files, updated index.html

--- init.bat
+++ init.bat
@@ -1,2 +1,6 @@
-venv\\scripts\\activate
+REM use this to set up actual program
 pip install -r requirements.txt
+cd FFR
+manage.py makemigrations
+manage.py migrate
+manage.py runserver
---
+++ venv_init.bat
@@ -0,0 +1,2 @@
+REM use this to set up virtual environment
+venv\\scripts\\activate
