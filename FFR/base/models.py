25b0163adfe5ed230c6b95acefebf4e2f2a6a540
Jordan Koelbl <jkoelbl@csumb.edu>
2018-08-23 18:59:34 -0700

    early stages development - database implementation

--- FFR/base/models.py
+++ FFR/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class story(models.Model):
+	title = models.TextField()
+	author = models.CharField(max_length=256)
+	content = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now_add=True)
+	upvotes = models.IntegerField(default=0)
+	downvotes = models.IntegerField(default=0)
+	
