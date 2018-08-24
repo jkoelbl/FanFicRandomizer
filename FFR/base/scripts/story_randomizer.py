9e7ff4c75991150839d947aa9dc2308f9cd567bd
Jordan Koelbl <jkoelbl@csumb.edu>
2018-08-23 17:45:29 -0700

    front and backend respond to each other, slight change to core functionality, removed output.html

--- FFR/base/scripts/story_randomizer.py
+++ FFR/base/scripts/story_randomizer.py
@@ -1,23 +1,21 @@
 from os import listdir, getcwd
 from random import randint
 
-mypath = getcwd() + '\\stories'
+mypath = getcwd() + '\\base\\stories'
 byte_size = 256
 
-#get and return filenames in stories path
+# get and return filenames in stories path
 def get_filenames():
 	return [mypath + '\\' + f for f in listdir(mypath)]
-	
-#read file by every 256 bytes to save on mem
-def read_file(filename):
-	with open(filename) as file:
-		temp = file.read(byte_size)
-		while temp:
-			print(temp, end='')
-			temp = file.read(byte_size)
 
-#get filenames, pick one at random, and read it
-def get_random_story():
+# get filenames, picks one at random, and returns it
+def select_random_file():
 	filenames = get_filenames()
 	index = randint(0,len(filenames)-1)
 	return filenames[index]
+
+# reads random file as a whole
+def get_random_story():
+	filename = select_random_file()
+	with open(filename) as file:
+		return file.read()
--- FFR/base/views.py
+++ FFR/base/views.py
@@ -3,10 +3,5 @@ from .scripts import *
 
 # Create your views here.
 def index(response):
-	return render(response, 'index.html')
-
-def output(request):
-	if request.is_ajax():
-		filename = get_random_story()
-		with open('stories\\' + filename) as file:
-			return render(request, 'index.html', {'output': file.read()})
+	return render(response, 'index.html', \
+		{'output': get_random_story()})
